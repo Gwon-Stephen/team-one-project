aab87de2ae4f47dd2646622d40b7331fa155a696
Stephen Gwon <gwonstephen@gmail.com>
2022-07-05 11:00:48 -0400

    quality of life
    asdf

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -15,7 +15,7 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown (KeyCode.Escape))
+        if(Input.GetKeyDown ("f"))
             doMovement = !doMovement;
 
         if(!doMovement)
