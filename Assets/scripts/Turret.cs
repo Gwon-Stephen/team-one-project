75398831bba938520e3264cdc64fd61eb31e31d5
bfayad <braydenfayad@gmail.com>
2022-07-07 08:04:14 -0400

    Revert "added in hannahs art assets and some text"
    This reverts commit 534e34fb48ea3739ded656357240aac118a45ca2.

--- Assets/scripts/Turret.cs
+++ Assets/scripts/Turret.cs
@@ -65,8 +65,8 @@ public class Turret : MonoBehaviour
         //target lockon
         Vector3 dir = target.position - transform.position;
         Quaternion lookRotation = Quaternion.LookRotation(dir);
-        //Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation ,Time.deltaTime*turnSpeed).eulerAngles;
-        //partToRotate.rotation = Quaternion.Euler (0f, rotation.y, 0f);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation ,Time.deltaTime*turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler (0f, rotation.y, 0f);
         
         if(fireCountdown <= 0f)
         {
